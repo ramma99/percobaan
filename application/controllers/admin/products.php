71b261cee1642570d22ac4d3dd3dbcc95a3f34fd
ramma99 <55478759+ramma99@users.noreply.github.com>
2020-03-17 22:21:17 +0700

    firts

---
+++ application/controllers/admin/products.php
@@ -0,0 +1,111 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Products extends CI_Controller {
+	public function __construct(){
+		parent::__construct();
+		
+		if($this->session->userdata('group') != '1'){
+			$this->session->set_flashdata('error','Sorry, you are not logged in!');
+			redirect('login');
+		}
+		
+		//load model -> model_products
+		$this->load->model('model_products');
+	}
+	
+	public function index()
+	{
+		$data['products'] = $this->model_products->all();
+		$this->load->view('backend/view_all_products', $data);
+	}
+	
+	public function create(){
+		//form validation sebelum mengeksekusi QUERY INSERT
+		$this->form_validation->set_rules('name', 'Product Name', 'required');
+		$this->form_validation->set_rules('description', 'Product Description', 'required');
+
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('backend/form_tambah_product');
+		} else {
+			//load uploading file library
+			$config['upload_path'] = './uploads/';
+			$config['allowed_types'] = 'jpg|png';
+			$config['max_size']	= '3000000'; //MB
+			$config['max_width']  = '30000000'; //pixels
+			$config['max_height']  = '30000000'; //pixels
+
+			$this->load->library('upload', $config);
+			
+			if ( ! $this->upload->do_upload())
+			{
+				//file gagal diupload -> kembali ke form tambah
+				$this->load->view('backend/form_tambah_product');
+			} else {
+				//file berhasil diupload -> lanjutkan ke query INSERT
+				// eksekusi query INSERT
+				$gambar = $this->upload->data();
+				$data_product =	array(
+					'name'			=> set_value('name'),
+					'description'	=> set_value('description'),
+					'image'			=> $gambar['file_name']
+				);
+				$this->model_products->create($data_product);
+				redirect('admin/products');
+			}
+			
+		}
+	}
+	
+	public function update($id){
+		$this->form_validation->set_rules('name', 'Product Name', 'required');
+		$this->form_validation->set_rules('description', 'Product Description', 'required');
+
+		if ($this->form_validation->run() == FALSE)
+		{
+			$data['product'] = $this->model_products->find($id);
+			$this->load->view('backend/form_edit_product', $data);
+		} else {
+			if($_FILES['userfile']['name'] != ''){
+				//form submit dengan gambar diisi
+				//load uploading file library
+				$config['upload_path'] = './uploads/';
+				$config['allowed_types'] = 'jpg|png';
+				$config['max_size']	= '30000'; //KB
+				$config['max_width']  = '300000'; //pixels
+				$config['max_height']  = '300000'; //pixels
+
+				$this->load->library('upload', $config);
+			
+			
+				if ( ! $this->upload->do_upload())
+				{
+					$data['product'] = $this->model_products->find($id);
+					$this->load->view('backend/form_edit_product', $data);
+				} else {
+					$gambar = $this->upload->data();
+					$data_product =	array(
+						'name'			=> set_value('name'),
+						'description'	=> set_value('description'),
+						'image'			=> $gambar['file_name']
+					);
+					$this->model_products->update($id, $data_product);
+					redirect('admin/products');
+				}
+			} else {
+				//form submit dengan gambar dikosongkan
+				$data_product =	array(
+					'name'			=> set_value('name'),
+					'description'	=> set_value('description'),
+				);
+				$this->model_products->update($id, $data_product);
+				redirect('admin/products');
+			}
+		}
+	}
+	
+	public function delete($id){
+		$this->model_products->delete($id);
+		redirect('admin/products');
+	}
+}
